9494d5ee8134625bb68d7805fcd19967f141cf74
Aziz Bedouihech <abedouihech93@gmail.com>
2021-07-03 13:20:33 +0100

    first commit

---
+++ DML.sql
@@ -0,0 +1,39 @@
+INSERT INTO Product VALUES
+(
+'P01',
+'Samsung Galaxy S20',
+'Smartphone',
+3299
+);
+INSERT INTO Product VALUES
+(
+'P02',
+'ASUS Notebook',
+'PC',
+4599
+);
+INSERT INTO Custumer VALUES
+(
+'C01',
+'Ali',
+71321009
+);
+INSERT INTO Custumer VALUES
+(
+'C02',
+'Asma',
+77345823
+);
+INSERT INTO Orders(Custumer_id,Product_id,Quantity,Total_amount) VALUES
+(
+'C01',
+'P02',
+2,
+9198
+);INSERT INTO Orders VALUES
+(
+'C02',
+'28/05/2020',
+1,
+3299
+);
